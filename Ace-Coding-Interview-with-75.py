b9115916b89bf4e0e4ac46049ed36b0c11106b5f
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-04-27 17:00:38 +0800

    Add Merge Strings Alternately

---
+++ Ace-Coding-Interview-with-75.py
@@ -0,0 +1,13 @@
+# Merge Strings Alternately
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
+        res = ""
+        while i < len(word1) and j < len(word2):
+            res += word1[i] + word2[j]
+            i += 1
+            j += 1
+
+        # add the additional letter before
+        res += word1[i:] + word2[j:]
+        return res
