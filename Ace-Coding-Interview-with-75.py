192edd37d223390e4a42751fdc5ccccc819d9b86
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-04-29 10:11:28 +0800

    Update Ace-Coding-Interview-with-75.py

--- Ace-Coding-Interview-with-75.py
+++ Ace-Coding-Interview-with-75.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # Merge Strings Alternately
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
@@ -43,3 +45,72 @@ class Solution:
                     return True
                 i -= 1
         return False
+
+    def reverseVowels(self, s: str) -> str:
+        s=list(s)
+        n=len(s)
+        left=0
+        right=n-1
+        # Use hashset can reduce to O(1) checking
+        vowels=set('AEIOUaeiou')
+        while left<right:
+            while left<right and s[left] not in vowels:
+                left+=1
+            while left<right and s[right] not in vowels:
+                right-=1
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
+        s=''.join(s)
+        return s
+    
+    # def reverseWords(self, s: str) -> str:
+    #     return " ".join(reversed(s.split()))
+    
+    # https://builtin.com/data-science/pythonic
+    def reverseWords(self, s: str) -> str:
+        x = s.split()
+        # Can change into below but the speed is in same way
+        # python still need to store an intermediate variable
+        # return " ".join(s.split()[::-1])
+        return " ".join(x[::-1])
+    
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix_products = [1] * n
+        suffix_products = [1] * n
+        answer = [1] * n
+
+        # Calculate prefix products
+        for i in range(1, n):
+            prefix_products[i] = prefix_products[i - 1] * nums[i - 1]
+
+        # Calculate suffix products
+        for i in range(n - 2, -1, -1):
+            suffix_products[i] = suffix_products[i + 1] * nums[i + 1]
+
+        # Calculate answer
+        for i in range(n):
+            answer[i] = prefix_products[i] * suffix_products[i]
+
+        return answer
+    
+    # Faster version
+    # def productExceptSelf(self, nums: List[int]) -> List[int]:
+    #     arr = [1] * (len(nums))
+    #     pre_product = 1
+    #     for i in range(len(nums)):
+    #         arr[i] *= pre_product
+    #         pre_product *= nums[i]
+        
+    #     post_product = 1
+    #     for i in range(len(nums)-1, -1, -1):
+    #         arr[i] *= post_product
+    #         post_product *= nums[i]
+
+    #     return arr
+
+if __name__ == '__main__':
+    solution_instance = Solution()  # Create an instance of the Solution class
+    print(solution_instance.productExceptSelf([1,2,3,4]))  # Output: "world hello"
+
