d6e2e42dc9c38620aa07fe9b5f51e80b457ca3e9
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-05-03 15:41:55 +0800

    Update hot100.py

--- hot100.py
+++ hot100.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, ListNode, Optional
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -32,3 +32,27 @@ class Solution:
         result = dummyHead.next
         dummyHead.next = None
         return result
+    
+    # Faster Version
+    # def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    #     dummy = ListNode()
+    #     res = dummy
+
+    #     total = carry = 0
+
+    #     while l1 or l2 or carry:
+    #         total = carry
+
+    #         if l1:
+    #             total += l1.val
+    #             l1 = l1.next
+    #         if l2:
+    #             total += l2.val
+    #             l2 = l2.next
+
+    #         num = total % 10
+    #         carry = total // 10
+    #         dummy.next = ListNode(num)
+    #         dummy = dummy.next
+        
+    #     return res.next
