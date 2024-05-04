9dc4162fa5ed4580a78d2fb2f6a1f823798fc8a0
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-05-04 23:20:06 +0800

    Update hot100.py

--- hot100.py
+++ hot100.py
@@ -1,4 +1,4 @@
-from typing import List, ListNode, Optional
+from typing import List, Optional
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -9,29 +9,29 @@ class Solution:
                 return [i, pair_idx[target - num]]
             pair_idx[num] = i
 
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummyHead = ListNode(0)
-        tail = dummyHead
-        carry = 0
+    # def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    #     dummyHead = ListNode(0)
+    #     tail = dummyHead
+    #     carry = 0
 
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
+    #     while l1 is not None or l2 is not None or carry != 0:
+    #         digit1 = l1.val if l1 is not None else 0
+    #         digit2 = l2.val if l2 is not None else 0
 
-            sum = digit1 + digit2 + carry
-            digit = sum % 10
-            carry = sum // 10
+    #         sum = digit1 + digit2 + carry
+    #         digit = sum % 10
+    #         carry = sum // 10
 
-            newNode = ListNode(digit)
-            tail.next = newNode
-            tail = tail.next
+    #         newNode = ListNode(digit)
+    #         tail.next = newNode
+    #         tail = tail.next
 
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
+    #         l1 = l1.next if l1 is not None else None
+    #         l2 = l2.next if l2 is not None else None
 
-        result = dummyHead.next
-        dummyHead.next = None
-        return result
+    #     result = dummyHead.next
+    #     dummyHead.next = None
+    #     return result
     
     # Faster Version
     # def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
@@ -56,3 +56,37 @@ class Solution:
     #         dummy = dummy.next
         
     #     return res.next
+
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     seen = {}
+    #     l = 0
+    #     length = 0
+    #     for r in range(len(s)):
+    #         char = s[r]
+    #         if char in seen and seen[char] >= l:
+    #             l = seen[char] + 1
+    #         else:
+    #             length = max(length, r - l + 1)
+    #         seen[char] = r
+
+    #     return length
+    
+    # Faster Version
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charset = set()
+        maxlength=0
+        l=0
+        for r in range(len(s)):
+            if s[r] not in charset:
+                charset.add(s[r])
+                maxlength = max(maxlength, r-l+1)
+            else:
+                while s[r] in charset:
+                    charset.remove(s[l])
+                    l+=1
+                charset.add(s[r])
+        return maxlength
+
+if __name__ == '__main__':
+    solution_instance = Solution()  # Create an instance of the Solution class
+    print(solution_instance.lengthOfLongestSubstring("pwwkew"))  # Output: "world hello"
