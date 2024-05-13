bf7bbcda743b6402b2ed54de1521426200d2bef4
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-05-13 10:37:24 +0800

    Create combinationSum.py

---
+++ top-liked-100/combinationSum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+        dp = [[] for _ in range(target + 1)]
+
+        for c in candidates:
+            print("char: ", c)
+            for i in range(c, target + 1):
+                print("target: ", target, "--dp: ", dp, "i: ", i , " Before")
+                if i == c:
+                    print("i == c: ", i)
+                    dp[i].append([c])
+                for comb in dp[i - c]:
+                    print("comb: ", comb, "[c]: ", [c])
+                    dp[i].append(comb + [c])
+                print("target: ", target, "--dp: ", dp, "i: ", i, "After")
+        return dp[target]
+    
+
+if __name__ == '__main__':
+    solution_instance = Solution()  # Create an instance of the Solution class
+    print(solution_instance.combinationSum([2,3,6,7], 7))
