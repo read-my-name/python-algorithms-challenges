fb16b10ec7a4ec2a86f8acb31087b825fab244cf
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-05-21 10:45:02 +0800

    Create findMedianSortedArrays.py

---
+++ hot100/findMedianSortedArrays.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1.sort()
+        m=len(nums1)//2
+        if len(nums1)%2!=0:
+            return nums1[m]
+        else:
+            return (nums1[m-1]+nums1[m])/2
+        
+if __name__ == '__main__':
+    solution_instance = Solution()  # Create an instance of the Solution class
+    print(solution_instance.findMedianSortedArrays([1,3],[2]))
