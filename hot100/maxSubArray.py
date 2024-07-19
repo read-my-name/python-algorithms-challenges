75c064797aa74f7a7e18de7fc468f0d5eacf5a5c
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-07-19 14:51:56 +0800

    Update maxSubArray.py

--- hot100/maxSubArray.py
+++ hot100/maxSubArray.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxSum = float('-inf')
@@ -13,3 +15,9 @@ class Solution:
                 currentSum = 0
         
         return maxSum
+    
+if __name__ == "__main__":
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    print(Solution().maxSubArray(nums))
+    # 6
+    # Explanation: [4,-1,2,1] has the largest sum = 6.
