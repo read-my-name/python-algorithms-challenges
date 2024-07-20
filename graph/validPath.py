513aca72ee04679f7d7d2120f4af39e406a0d794
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-07-20 11:51:54 +0800

    Update validPath.py

--- graph/validPath.py
+++ graph/validPath.py
@@ -2,19 +2,28 @@ from typing import List
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        parent=list(range(n))
+        parent = list(range(n))
+        
+        # Function to find the root of node i
         def find(i):
-            if parent[i]==i: 
+            if parent[i] == i: 
                 return i
             else:
-                res=find(parent[i])
-                parent[i]=res
+                res = find(parent[i])
+                parent[i] = res  # Path compression
                 return res
-        def union(i,j): # j is always on top
-            parent[find(i)]=find(j)
+
+        # Function to union two nodes i and j
+        def union(i, j): 
+            parent[find(i)] = find(j)  # Always attach tree of i to tree of j
+
+        # Union all edges
         for e in edges:
-            union(e[0],e[1])
-        return find(source)==find(destination)
+            union(e[0], e[1])
+        
+        # Check if source and destination have the same root
+        return find(source) == find(destination)
+
     
 if __name__ == '__main__':
     n = 3
