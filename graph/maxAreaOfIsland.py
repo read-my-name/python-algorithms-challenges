c2d86704e0b04918a6a913c294b16961d43c4a24
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-07-23 09:11:11 +0800

    Create maxAreaOfIsland.py

---
+++ graph/maxAreaOfIsland.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 \
+                or j >= len(grid[0]) \
+                or grid[i][j] != 1:
+                return 0
+            grid[i][j] = 0
+            area = 1
+            area += dfs(i + 1, j)
+            area += dfs(i - 1, j)
+            area += dfs(i, j + 1)
+            area += dfs(i, j - 1)
+            return area
+
+        max_area = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    max_area = max(max_area, dfs(i, j))
+        return max_area
+    
+if __name__ == "__main__":
+    #grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],]
+    sol = Solution()
+    grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+            [0,0,0,0,0,0,0,1,1,1,0,0,0],
+            [0,1,1,0,1,0,0,0,0,0,0,0,0],
+            [0,1,0,0,1,1,0,0,1,0,1,0,0],
+            [0,1,0,0,1,1,0,0,1,1,1,0,0],
+            [0,0,0,0,0,0,0,0,0,0,1,0,0],
+            [0,0,0,0,0,0,0,1,1,1,0,0,0],
+            [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+    print(sol.maxAreaOfIsland(grid))
