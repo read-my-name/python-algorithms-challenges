b06d9282f38ccce8053fc5f744b191db25043fc8
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-07-29 11:32:53 +0800

    Update maximalRectangle.py

--- hot100/maximalRectangle.py
+++ hot100/maximalRectangle.py
@@ -11,17 +11,25 @@ class Solution:
         max_area = 0
         
         for row in matrix:
+            print("=======================")
+            # Update heights for the current row
+            print("row: ", row)
             for i in range(cols):
                 heights[i] = heights[i] + 1 if row[i] == '1' else 0
+            print("heights: ", heights) 
             
             # Calculate max area using histogram method
             stack = []
             for i in range(len(heights)):
+                print("i: ", i, "heights[i]: ", heights[i], "stack: ", stack)
                 while stack and heights[i] < heights[stack[-1]]:
+                    print("=== heights[i] < heights[stack[-1]]: ", heights[i], " < ", heights[stack[-1]])
                     h = heights[stack.pop()]
                     w = i if not stack else i - stack[-1] - 1
                     max_area = max(max_area, h * w)
+                    print("max_area: ", max_area, "h: ", h, "w: ", w)
                 stack.append(i)
+            print("=======================")
         
         return max_area
     
@@ -29,7 +37,7 @@ if __name__ == "__main__":
     matrix = [
         ["1", "0", "1", "0", "0"],
         ["1", "0", "1", "1", "1"],
-        ["1", "1", "1", "1", "1"],
+        ["1", "0", "1", "1", "1"],
         ["1", "0", "0", "1", "0"]
     ]
     solution = Solution()
