7fce07708b2ca1700722fd09dc98bcf03830e2b9
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-08-04 22:35:30 +0800

    Create letterCombinations.py

---
+++ hot100/letterCombinations.py
@@ -0,0 +1,32 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        keypad = {
+            '2':'abc',
+            '3':'def',
+            '4':'ghi',
+            '5':'jkl',
+            '6':'mno',
+            '7':'pqrs',
+            '8':'tuv',
+            '9':'wxyz'
+        }
+        arr = []
+        def dfs(res,string):
+            if not string:
+                arr.append(res)
+                return
+            currNum = string[0]
+            for i in range(len(keypad[currNum])):
+                dfs(res+keypad[currNum][i],string[1:])
+        if digits:
+            dfs("",digits)
+        return arr
+
+if __name__ == "__main__":
+    print(Solution().letterCombinations("23"))
+    print(Solution().letterCombinations(""))
+    print(Solution().letterCombinations("2"))
+    print(Solution().letterCombinations("234"))
+    print(Solution().letterCombinations("2345"))
+
+
