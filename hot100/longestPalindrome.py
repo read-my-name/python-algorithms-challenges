7d042abb4b253c1e0b9434b811cd05539f5b28ad
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-08-04 22:35:25 +0800

    Update longestPalindrome.py

--- hot100/longestPalindrome.py
+++ hot100/longestPalindrome.py
@@ -2,30 +2,20 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if s[::-1] == s:
             return s
-        
-        # inti start at 0 and window size is 1
         start, size = 0, 1
-
         for i in range(1, len(s)):
-            # set up l, r pointer , base on cur idx to left
-            # cur window size = max window size + 1
-            l, r = i - size, i + 1
+            left, right = i - size, i + 1
+            s1 = s[left-1:right]
 
-            # cur window size = max window size + 2
-            s1 = s[l-1:r]
-
-            # if s1 is palindrome, update start idx and the size
-            if l >= 1 and s1[::-1] == s1:
+            if left >= 1 and s1[::-1] == s1:
                 size += 2
-                start = l - 1
+                start = left - 1
             else:
-                # cur window size = max window size + 1
-                s2 = s[l:r]
+                s2 = s[left: right]
                 if s2[::-1] == s2:
                     size += 1
-                    start = l
-        
-        return s[start:start+size]
+                    start = left
+        return s[start:start + size]
     
 if __name__ == '__main__':
     solution_instance = Solution()  # Create an instance of the Solution class
