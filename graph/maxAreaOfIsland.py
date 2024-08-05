16b9139e15054092b8cf2233b35432cf71f1a6f3
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-08-05 23:36:38 +0800

    695. Max Area of Island

--- graph/maxAreaOfIsland.py
+++ graph/maxAreaOfIsland.py
@@ -3,8 +3,8 @@ from typing import List
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 \
-                or j >= len(grid[0]) \
+            if i < 0 or i >= len(grid) or \
+                j < 0 or j >= len(grid[0]) \
                 or grid[i][j] != 1:
                 return 0
             grid[i][j] = 0
