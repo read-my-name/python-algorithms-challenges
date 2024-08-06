2ffb01a30cc2d2646bddea4795445b9622206c85
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-08-06 14:55:24 +0800

    1. Two Sum

--- hot100/twoSum.py
+++ hot100/twoSum.py
@@ -9,7 +9,7 @@ class Solution:
                 return [i, pair_idx[target - num]]
             pair_idx[num] = i
 
-
+# 1. Two Sum
 if __name__ == '__main__':
     solution_instance = Solution()  # Create an instance of the Solution class
     print(solution_instance.twoSum([2, 7, 11, 15], 9))
