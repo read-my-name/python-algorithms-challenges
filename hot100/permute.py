166750a50cd77576ebf4d94ba142930c1a8d8fb0
read-my-name <116362889+read-my-name@users.noreply.github.com>
2024-08-13 11:41:53 +0800

    46. Permutations

---
+++ hot100/permute.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1:
+            return [nums[:]]
+        res = []
+        for _ in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for p in perms:
+                p.append(n)
+            
+            res.extend(perms)
+            nums.append(n)
+        
+        return res
+
+if __name__ == "__main__":
+    nums = [1,2,3]
+    print(Solution().permute(nums))
+    nums = [0,1]
+    print(Solution().permute(nums))
+    nums = [1]
+    print(Solution().permute(nums))
+    
